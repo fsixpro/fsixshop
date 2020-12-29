f411c7dd393212439666f67e6b5e05fec967e4f5
Francis Ayodeji <fsixpro@gmail.com>
2020-12-29 18:30:33 +0100

    testing

--- server.js
+++ server.js
@@ -26,6 +26,18 @@ app.use('/api/upload', uploadRoute)
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
 const PORT = process.env.PORT
 app.listen(5000, () =>
   console.log(
